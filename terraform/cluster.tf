b939e2a3baca0453df5dd4d5f43511f66df5003e
KryptonGuy <yash.sant.orignal@gmail.com>
2023-07-03 09:50:41 +0530

    Added Support for code build

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -2,7 +2,7 @@ module "eks" {
     source  = "terraform-aws-modules/eks/aws"
     version = "~> 19.0"
     cluster_name = "login-systems"
-    cluster_version = "1.25"
+    cluster_version = "1.27"
 
     cluster_endpoint_public_access  = true
 
